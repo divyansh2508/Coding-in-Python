db65584876bdc740c176acdbf5dbe4cead670b27
Divyansh <86479436+divyansh2508@users.noreply.github.com>
2022-08-08 15:59:01 +0530

    Add files via upload
    Swapping of 2 numbers without using the 3rd variable.

---
+++ swapping.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+print("The original values of a and b are",a,b)
+a=a+b
+b=a-b
+a=a-b
+print("The exchanged values of a and b are",a,b)
